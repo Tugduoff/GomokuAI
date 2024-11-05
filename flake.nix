c5b85a8c1bbf908a4e39ea9aac216734ada10f4c
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-11-05 13:51:45 +0100

    :heavy_plus_sign: Added flake for nix users on the project.

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = '':
+    Gomoku Flake
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self , nixpkgs ,... }:
+    let
+      system = "x86_64-linux";
+    in {
+      devShells."${system}".default = let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in pkgs.mkShell {
+        packages = with pkgs; [
+          bashInteractiveFHS
+          pkg-config
+
+          python3
+        ];
+
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.wayland}/lib:$LD_LIBRARY_PATH
+          echo Custom cpp nix-shell !
+        '';
+      };
+    };
+}
