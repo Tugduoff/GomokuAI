c2df253ad915bb2e5bb6b91ff7576cdff6eb07cb
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-11-05 16:54:28 +0100

    :sparkles: Added a new board class with default member functions for it.

---
+++ models/Board.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+#
+# EPITECH PROJECT, 2024
+# GomokuAI
+# File description:
+# Board class
+#
+
+import array
+
+class Board:
+    def __init__(self):
+        """
+            Constructor of the Board class
+            Initializes the board with a size of 19x19 (hardcoded)
+        """
+        self.boardSize = 19
+        self.board = None
+        self.initBoard()
+
+    def get_board(self):
+        """
+            Getter of the board attribute
+        """
+        return self.board
+
+    def set_board(self, board: array):
+        """
+            Setter of the board attribute
+        """
+        self.board = board
+
+    def get_board_size(self):
+        """
+            Getter of the boardSize attribute
+        """
+        return self.boardSize
+
+    def set_board_size(self, size: int):
+        """
+            Setter of the boardSize attribute
+        """
+        self.boardSize = size
+        self.initBoard()
+
+    def get_board_value(self, x: int, y: int):
+        return self.board[x][y]
+
+    def set_board_value(self, x: int, y: int, value: int):
+        self.board[x][y] = value
+
+    def initBoard(self):
+        """
+            Initializes the board with boardSize x boardSize dimensions
+        """
+        self.board = [[0 for _ in range(self.boardSize)] for _ in range(self.boardSize)]
+        self.fillBoard()
+
+    def fillBoard(self):
+        """
+            Fills the board with 0's
+        """
+        for i in range(self.boardSize):
+            for j in range(self.boardSize):
+                self.board[i][j] = 0
+
+    def is_valid_move(self, x, y):
+        """
+            Checks if the move is within bounds and the cell is empty.
+        """
+        if 0 <= x < self.boardSize and 0 <= y < self.boardSize:
+            return self.board[x][y] == 0
+        return False
+
+    def display_board(self):
+        """
+            Displays the board in a readable format.
+        """
+        if self.board is not None:
+            for row in self.board:
+                print(" ".join(str(cell) for cell in row))
+        else:
+            print("Board is empty")
