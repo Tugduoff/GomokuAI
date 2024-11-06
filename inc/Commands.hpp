184d553d7d69218ca097eca437eecb9a530b270e
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-11-06 17:39:21 +0100

    :heavy_plus_sign: Added the Commands class

--- inc/Commands.hpp
+++ inc/Commands.hpp
@@ -5,13 +5,90 @@
 ** Commands
 */
 
-#pragma once
+#ifndef COMMANDS_HPP
+    #define COMMANDS_HPP
 
-class Commands {
-    public:
-        Commands();
-        ~Commands();
+    #include <iostream>
+    #include <map>
+    #include <string>
+    #include <array>
 
-    protected:
-    private:
-};
+    class Commands {
+        public:
+            /**
+             * @brief Construct a new Commands object
+             *
+             */
+            Commands()
+            {
+            }
+
+            /**
+             * @brief Destroy the Commands object
+             *
+             */
+            ~Commands();
+
+            /**
+             * @brief Return the command from a line
+             *
+             * @param std::string line
+             * @return std::string
+             */
+            std::string getCommand(std::string line)
+            {
+                std::string command = line.substr(0, line.find(" "));
+                return command;
+            }
+
+            /**
+             * @brief Awnser to the start command
+             *
+             * @param std::map<std::string, int8_t> infoDict
+             */
+            void startCommand(void)
+            {
+                std::cout << "OK" << std::endl;
+            }
+
+            /**
+             * @brief Awnser to the begin command
+             *
+             * @param std::map<std::string, int8_t> infoDict
+             */
+            void turnCommand(int8_t x = 1, int8_t y = 1)
+            {
+                std::cout << x << "," << y << std::endl;
+            }
+
+            /**
+             * @brief Awnser to the board command
+             *
+             * @param std::map<std::string, int8_t> infoDict
+             */
+            void boardCommand(int8_t x = 1, int8_t y = 1)
+            {
+                turnCommand(x, y);
+            }
+
+            /**
+             * @brief Awnser to the info command
+             *
+             * @param std::map<std::string, int8_t> infoDict
+             */
+            void infoCommand(std::string info)
+            {
+                return;
+            }
+
+            void aboutCommand(void)
+            {
+                std::cout << "name=\"Centurion\", version=\"1.0\", author=\"Tugdu & Traz\", country=\"France\"" << std::endl;
+            }
+
+
+        protected:
+        private:
+    };
+
+#endif
