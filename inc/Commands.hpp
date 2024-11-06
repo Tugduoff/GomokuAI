a4cfd315542fea1a88a94fa6cba657096049a980
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-11-06 16:44:52 +0100

    :heavy_plus_sign: Added include folder

---
+++ inc/Commands.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** GomokuAI
+** File description:
+** Commands
+*/
+
+#pragma once
+
+class Commands {
+    public:
+        Commands();
+        ~Commands();
+
+    protected:
+    private:
+};
