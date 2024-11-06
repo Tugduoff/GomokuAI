25db5c194d950989d8a1dd9df5e4555cfb9c687c
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-11-06 17:40:45 +0100

    :heavy_plus_sign: Added the Commands variable and updated the main

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,17 +8,20 @@
 #include <string>
 #include <iostream>
 #include "Board.hpp"
+#include "Commands.hpp"
 
 Gomoku::Board board;
 
 int main(void)
 {
+    Commands commands;
     std::string line;
 
     while (1) {
         std::cin >> line;
 
         if (line == "START") {
+            commands.startCommand();
             uint8_t size = 20;
             std::cin >> size;
         }
