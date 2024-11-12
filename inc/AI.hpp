6910edad7b90a82c9852818e8e8aea3e07243eed
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-11-12 15:55:03 +0100

    :heavy_minus_sign: Removed old functions

--- inc/AI.hpp
+++ inc/AI.hpp
@@ -517,117 +517,6 @@ namespace Gomoku {
             }
 
             /**
-             * @brief Check if the player of the color given has won
-             * 
-             * @param color
-             * @param x
-             * @param y
-             * 
-             * @return true : the player has won
-             * @return false : the player has not won
-             * 
-             * This function checks if the player has won
-             */
-            bool checkWin(uint8_t color, uint8_t x, uint8_t y) {
-                if ((y <= 15 && checkFiveInRow(x, y, 0, 1, color)) ||
-                    (x <= 15 && checkFiveInRow(x, y, 1, 0, color)) ||
-                    (x <= 15 && y <= 15 && checkFiveInRow(x, y, 1, 1, color)) ||
-                    (x >= 4 && y <= 15 && checkFiveInRow(x, y, -1, 1, color)))
-                    return true;
-                return false;
-            }
-
-            /**
-             * @brief Check if the player of the color given has a threat in the board
-             * 
-             * @param color
-             * @param x
-             * @param y
-             * 
-             * @return true : the player has won
-             * @return false : the player has not won
-             * 
-             * This function checks if the player has won
-             */
-            void checkThreat(uint8_t color, uint8_t x, uint8_t y) {
-                int colorEval = color == 1 ? 1 : -1;
-                int threatScore = 0;
-                std::vector<Position> threatStones;
-                int direction = 0; // 0: horizontal, 1: vertical, 2: diagonal, 3: anti-diagonal
-
-                for (int i = 1; i < 6; ++i) {
-                    if ((y <= 20 - i && checkNInRow(x, y, 0, 1, color, i))) {
-                        threatScore += 100 * i * colorEval;
-                        threatStones.push_back(Position(x, y + i - 1));
-                        direction = 1;
-                    } else if (x <= 20 - i && checkNInRow(x, y, 1, 0, color, i)) {
-                        threatScore += 100 * i * colorEval;
-                        threatStones.push_back(Position(x + i - 1, y));
-                        direction = 0;
-                    } else if (x <= 20 - i && y <= 20 - i && checkNInRow(x, y, 1, 1, color, i)) {
-                        threatScore += 100 * i * colorEval;
-                        threatStones.push_back(Position(x + i - 1, y + i - 1));
-                        direction = 2;
-                    } else if (x >= i && y <= 20 - i && checkNInRow(x, y, -1, 1, color, i)) {
-                        threatScore += 100 * i * colorEval;
-                        threatStones.push_back(Position(x - i, y + i - 1));
-                        direction = 3;
-                    } else
-                        break;
-                }
-                if (threatStones.size() > 0) {
-                    __threats.push_back(std::make_pair(threatScore, threatStones));
-                }
-            };
-
-
-            /**
-             * @brief Remove duplicate threats
-             * 
-             * The goal of this function is to remove duplicate threats such as small threats contained by another bigger threat
-             * 
-             * Example: \n
-             * 1. 1000 : (1,1) (1,2) (1,3) \n
-             * 2. 300 : (1,1) (1,2) \n
-             * 
-             * The second threat is contained by the first one, so we remove it
-             * 
-             * This function will start by sorting the threats by their score (using absolute value)
-             * Then, iterate over the threats and check if the positions of the current threat are contained in another threat
-             * If they aren't, we add the threat to the unique threats vector
-             * 
-             * Finally, we set the threats vector to the unique threats vector to keep only the unique threats
-             */
-            void removeDuplicateThreats() {
-                std::vector<std::pair<int, std::vector<Position>>> uniqueThreats;
-
-                std::sort(__threats.begin(), __threats.end(), [](const auto& a, const auto& b) {
-                    return std::abs(a.first) > std::abs(b.first);
-                });
-
-                for (const auto &currentThreat : __threats) {
-                    bool isContained = false;
-                    const auto &currentPositions = currentThreat.second;
-
-                    for (const auto &uniqueThreat : uniqueThreats) {
-                        const auto &uniquePositions = uniqueThreat.second;
-
-                        if (std::all_of(currentPositions.begin(), currentPositions.end(), [&](const Position &pos) {
-                            return std::find(uniquePositions.begin(), uniquePositions.end(), pos) != uniquePositions.end();
-                        })) {
-                            isContained = true;
-                            break;
-                        }
-                    }
-
-                    if (!isContained)
-                        uniqueThreats.push_back(currentThreat);
-                }
-
-                __threats = uniqueThreats;
-            }
-
-            /**
              * @brief Display the threats
              * 
              * @param threats : the vector of threats
@@ -647,50 +536,6 @@ namespace Gomoku {
                     }
                     std::cout << "DEBUG Threat score: " << threat.first << " :\n" << positions;
                 }
-            }
-
-            /**
-             * @brief Check if there are 5 stones in a row
-             * 
-             * @param x : x position of the starting stone
-             * @param y : y position of the starting stone
-             * @param dx : x direction
-             * @param dy : y direction
-             * @param player : player color
-             * 
-             * @return true : there are 5 stones in a row
-             * @return false : there are not 5 stones in a row
-             * 
-             * This function checks if there are 5 stones in a row starting from the given position
-             */
-            bool checkFiveInRow(int x, int y, int dx, int dy, int player) {
-                for (int i = 0; i < 5; ++i) {
-                    if (board.board[x + i * dx][y + i * dy] != player)
-                        return false;
-                }
-                return true;
-            }
-
-            /**
-             * @brief Check if there are 4 stones in a row
-             * 
-             * @param x : x position of the starting stone
-             * @param y : y position of the starting stone
-             * @param dx : x direction
-             * @param dy : y direction
-             * @param player : player color
-             * 
-             * @return true : there are 4 stones in a row
-             * @return false : there are not 4 stones in a row
-             * 
-             * This function checks if there are 4 stones in a row starting from the given position
-             */
-            bool checkFourInRow(int x, int y, int dx, int dy, int player) {
-                for (int i = 0; i < 4; ++i) {
-                    if (board.board[x + i * dx][y + i * dy] != player)
-                        return false;
-                }
-                return true;
             }
 
             bool checkNInRow(int x, int y, int dx, int dy, uint8_t color, int n) {
@@ -731,6 +576,11 @@ namespace Gomoku {
                 return false;
             }
 
+            /**
+             * @brief Get the best move for the AI
+             * 
+             * @return std::pair<uint8_t, uint8_t> 
+             */
             std::pair<uint8_t, uint8_t> getBestMove() {
                 int bestScore = std::numeric_limits<int>::min();
                 std::pair<uint8_t, uint8_t> bestMove = std::make_pair(0, 0);
@@ -738,7 +588,7 @@ namespace Gomoku {
                 for (uint8_t x = 0; x < 20; ++x) {
                     for (uint8_t y = 0; y < 20; ++y) {
                         if (board.board[x][y] == 0) {
-                            //
+                            
                         }
                     }
                 }
