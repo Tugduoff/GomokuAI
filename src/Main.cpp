36375a0ef8f2497332a54569ffa57ec92c42a9d9
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-11-14 14:55:33 +0100

    :heavy_plus_sign: Added threading to the project

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,23 +6,57 @@
 */
 
 #include <sstream>
+#include <functional>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
 #include "CommandHandler.hpp"
+
+std::mutex mtx;
+std::condition_variable cv;
+std::condition_variable cv_done;
+std::string cmd;
+bool ready = false;
+
+
+void readCmd() {
+    while (true) {
+        std::string tempCmd;
+        std::cin.clear();
+        std::getline(std::cin, tempCmd);
+        std::unique_lock<std::mutex> lock(mtx);
+        cmd = tempCmd;
+        ready = true;
+        std::cout << "DEBUG Read command: " << tempCmd << std::endl;
+        cv.notify_one();
+    }
+}
+
+void executeCmd(Gomoku::CommandHandler &handler) {
+    while (true) {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [] { return ready; });
+        std::cout << "DEBUG Executing command: " << cmd << std::endl;
+        handler.execute(cmd);
+
+        ready = false;
+    }
+}
 
 int main(void) {
     Gomoku::AI ai;
     Gomoku::CommandHandler handler(ai);
     std::string cmd;
 
+    std::thread reader(readCmd);
+    std::thread executor(executeCmd, std::ref(handler));
+
     srand(time(NULL));
 
-    while (true) {
-        std::cin.clear();
-        cmd.clear();
-        if (!(std::cin >> cmd)) {
-            std::cout << "DEBUG End of input detected. Exiting" << std::endl;
-            break;
-        }
-        handler.execute(cmd);
-    }
+    reader.join();
+    executor.join();
     return 0;
 }
