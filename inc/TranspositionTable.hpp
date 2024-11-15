ed97090ea475bfa968d5770f272d6d74fa4ca324
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-11-15 18:24:33 +0100

    :heavy_plus_sign: Added Zobrist related

--- inc/TranspositionTable.hpp
+++ inc/TranspositionTable.hpp
@@ -8,7 +8,10 @@
 #pragma once
 
 #include <iostream>
+#include <random>
 #include <unordered_map>
+#include "Board.hpp"
+#include "Stone.hpp"
 #include "Position.hpp"
 
 namespace Gomoku {
@@ -19,7 +22,7 @@ namespace Gomoku {
      */
     class TranspositionTable {
         public:
-            TranspositionTable() = default;
+            TranspositionTable() {};
             TranspositionTable(Position pos, uint8_t val, uint8_t dep, uint8_t fl)
                 : position(pos), value(val), depth(dep), flag(fl) {};
             ~TranspositionTable() = default;
@@ -36,11 +39,39 @@ namespace Gomoku {
                 return *this;
             }
 
+            void initializeZobristTable() {
+                std::mt19937_64 rng(12345);
+                std::uniform_int_distribution<uint64_t> dist;
+
+                for (int i = 0; i < 20; i++) {
+                    for (int j = 0; j < 20; j++) {
+                        for (int k = 0; k < 2; k++) {
+                            zobristTable[i][j][k] = dist(rng);
+                        }
+                    }
+                }
+            }
+
+            uint64_t computeZobristHash(Board &exploratingBoard) {
+                uint64_t hash = 0;
+
+                for (int i = 0; i < 20; i++) {
+                    for (int j = 0; j < 20; j++) {
+                        if (exploratingBoard.board[i][j] != Color::EMPTY) {
+                            int player = (exploratingBoard.board[i][j] == Color::AI) ? 1 : 2;
+                            hash ^= zobristTable[i][j][player];
+                        }
+                    }
+                }
+                return hash;
+            }
+
             std::unordered_map<uint64_t, TranspositionTable> transpositionTable;
+            uint8_t zobristTable[20][20][2];
             Position position;
             int value;
             uint8_t depth;
-            uint8_t flag;
+            int8_t flag;
 
         protected:
         private:
